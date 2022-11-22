55c1f276a10fd48894ae45b6646672df87a10127
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2022-11-22 15:54:30 +0100

    init logger

--- src/main/java/it/unibas/vulkanttt/Main.java
+++ src/main/java/it/unibas/vulkanttt/Main.java
@@ -1,7 +1,11 @@
 package it.unibas.vulkanttt;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 }
